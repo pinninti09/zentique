9fd9b4ac9e7996fb916c2ce112222aca752f6a26
pinninti09 <154372183+pinninti09@users.noreply.github.com>
2025-07-02 23:51:46 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+
 
 // Only import Replit plugins conditionally
 const replitRuntimeErrorOverlay =
@@ -19,15 +19,6 @@ export default defineConfig({
     react(),
     ...(replitRuntimeErrorOverlay ? [replitRuntimeErrorOverlay()] : []),
     ...(replitCartographer ? [replitCartographer()] : []),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
   ],
   resolve: {
     alias: {
